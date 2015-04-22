89f0e86d11e9429c012b4e4a7942f7dd43fcfa7a
Nick Ham <nicholas.charles.ham@gmail.com>
2015-04-23 02:40:45 +1000

    setup gh-pages branch

---
+++ js/process_pre_tags.js
@@ -0,0 +1,27 @@
+/*
+	Has browser ignore leading tabs for <pre> code blocks.
+	modified from: http://stackoverflow.com/a/8505182/4525897
+*/
+
+var pre_elements = document.getElementsByTagName('pre');
+
+for (var i = 0; i < pre_elements.length; i++)
+{
+	pre_elements = document.getElementsByTagName('pre');
+	var content = pre_elements[i].innerHTML;
+
+	var tabs_to_remove = '';
+	while (content.indexOf('\t') == '0')
+	{
+		tabs_to_remove += '\t';
+		content = content.substring(1);
+	}
+
+	var re = new RegExp('\n' + tabs_to_remove, 'g');
+	content = content.replace(re, '\n');
+
+	if(content.lastIndexOf("\n")>0) 
+		content = content.substring(0, content.lastIndexOf("\n"));
+
+	pre_elements[i].outerHTML = '<pre class="' + pre_elements[i].className + '">' + content + '</pre>';
+}
